d75534b7c8cb84ca5b13fd30b1fe538cc16cc2e0
unknown <ozanbarisbakir@gmail.com>
2022-01-25 04:08:36 +0300

    Coffe Website Project is added

---
+++ Coffe-Website Frontend Project/script.js
@@ -0,0 +1,48 @@
+// responsive navbar toggle
+let toggleButton = document.getElementsByClassName('toggle-button')[0]
+let navbarLinks = document.getElementsByClassName('navbar-links')[0]
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+    //
+    window.onscroll = function() {
+      var currentScrollPos = window.pageYOffset;
+        if (currentScrollPos > 80) {
+          navbarLinks.classList.remove('active');
+        }
+    }
+    navbarLinks.classList.add("fade-nav-links");
+});
+
+/*
+
+"pop-up tek sefer gösterme" bakım aşamasında :)
+
+
+var modalKaybol = document.getElementById("kaybol");
+function engelle(){
+  modalKaybol.style.display = "none";
+}
+
+*/
+
+
+
+
+// scroll animation
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
